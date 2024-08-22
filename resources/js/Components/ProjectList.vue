a73d3f51449b2fdda528d0b03f2f460a0cee2fb1
Caio Lesnock <caio.dev@outlook.com>
2024-08-21 21:29:41 -0300

    Add projeto form prototype

--- resources/js/Components/ProjectList.vue
+++ resources/js/Components/ProjectList.vue
@@ -2,15 +2,35 @@
 import type { Project as ProjectType } from '@/types';
 import Project from './Project.vue';
 
-defineProps<{
+type Props = {
     projects: ProjectType[]
-}>()
+}
+
+withDefaults(defineProps<Props>(), {
+    projects: () => []
+})
 </script>
 
 <template>
-    <div class="projects-list">
+    <div class="p-6 text-gray-900 dark:text-gray-100 ">
         <div class="empty" v-if="!projects.length">
-            Você ainda não possui projetos.
+            <span class="text-lg">Adicionar Projeto</span>
+            <!-- Você ainda não possui projetos. -->
+            <input class="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Título do Projeto..." 
+                required />
+
+            <textarea class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-4" 
+                placeholder="Descrição do Projeto..." 
+                required />
+
+            <input type="datetime-local" class="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required />
+
+            <button type="button" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Salvar
+            </button>
+
         </div>
 
         <div class="projects">
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ProjectList from '@/Components/ProjectList.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
@@ -14,8 +15,13 @@ import { Head } from '@inertiajs/vue3';
                 <span>Projetos</span>
 
                 <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Add Projeto
+                    class="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="inline-block size-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+
+                    <span class="mt-0.5 ml-1">Novo Projeto</span>
                 </button>
             </h2>
         </template>
@@ -23,11 +29,7 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100 ">
-                        <span>
-                            Você ainda não possui projetos.
-                        </span>
-                    </div>
+                    <ProjectList />
                 </div>
             </div>
         </div>
